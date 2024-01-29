bb5fa40404e04df944e9d92ce6bade1e5d2f5f80
sibaram <sm60786@gmail.com>
2024-01-29 18:12:07 +0530

    Some sorting program has been commited for jenkins ci/cd demo purpose

---
+++ list_sibaram.py
@@ -0,0 +1,16 @@
+import os
+
+list1 = [7, 9, 7, 8, 0, 4]
+list1.reverse()
+print("The reversed List is : ", list1)
+
+list2 = [79, 78, 88, 11, 122, 900, 785, 222]
+list2.sort()
+print("The sorted List is : ", list2)
+
+list3 = []
+list3 = list1.copy()
+print("List3 : ", list3)
+
+idx_value = list2[0:2]
+print("The mentioned index values are in list2 : ", idx_value)
